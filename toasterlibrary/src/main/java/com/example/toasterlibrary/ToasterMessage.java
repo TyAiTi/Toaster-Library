f1963789534a5491a960b24ab7a9245fb8e31152
TyAiTi <35554335+TyAiTi@users.noreply.github.com>
2022-06-23 16:54:31 +0700

    feat (up move activity)

--- toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -1,11 +1,18 @@
 package com.example.toasterlibrary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class ToasterMessage {
 
     public static void s  (Context c, String message){
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void openActivity(Context c, String message){
+        Intent intent = new Intent(c, ToastActivity.class);
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+        c.startActivity(intent);
     }
 }
