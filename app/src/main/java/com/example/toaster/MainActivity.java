028c62d8821a6c804b10cdfe661d82ce4649651a
TyAiTi <35554335+TyAiTi@users.noreply.github.com>
2022-06-24 09:10:47 +0700

    feat (call Activity in library now)

--- app/src/main/java/com/example/toaster/MainActivity.java
+++ app/src/main/java/com/example/toaster/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.toaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.toasterlibrary.ToastActivity;
+import com.example.toasterlibrary.ToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnMoveLibrary;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMoveLibrary = findViewById(R.id.btn_move_library);
+
+
+        Intent intent = new Intent(this, ToastActivity.class);
+//        startActivity(intent);
+
+        btnMoveLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToasterMessage.openActivity(MainActivity.this, "Dasdasdasdasdas");
+            }
+        });
+
     }
 }
--- toasterlibrary/src/main/java/com/example/toasterlibrary/ToastActivity.java
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToastActivity.java
@@ -2,6 +2,7 @@ package com.example.toasterlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 public class ToastActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class ToastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
+    }
+
+    public void setBackgroundBmp(Bitmap bmp) {
+
     }
 }
